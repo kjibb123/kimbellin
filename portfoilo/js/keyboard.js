099ea9e707e4a68b8a639e0d80603834d02f66a4
kjibb123 <64953627+kjibb123@users.noreply.github.com>
2020-09-20 19:51:50 +0900

    bb
    bb

---
+++ portfoilo/js/keyboard.js
@@ -0,0 +1,22 @@
+const typedTextSpan = document.querySelector(".typed-text");
+
+
+const textArray = ["PROFILE"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type(){
+    if(charIndex < textArray[textArrayIndex].length){
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else{
+        //erase
+    }
+}
+
+type();
