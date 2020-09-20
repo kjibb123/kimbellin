d7bdf96dba2c6d4c5a5c88c72534237d56268ab3
kjibb123 <64953627+kjibb123@users.noreply.github.com>
2020-09-20 20:23:32 +0900

    gd
    gd

--- portfoilo/js/main.js
+++ portfoilo/js/main.js
@@ -164,3 +164,25 @@ $(function() {
 	  });
 });
 
+
+const typedTextSpan = document.querySelector(".typed-text");
+
+
+const textArray = ["PROFILE"];
+const typingDelay = 200;
+
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type(){
+    if(charIndex < textArray[textArrayIndex].length){
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else{
+        //erase
+    }
+}
+
+type();
